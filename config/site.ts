aa71fa911aace3d0b957ba6a538e3e3583b85bbe
mazzasaverio <saverio3107@gmail.com>
2024-04-07 18:52:58 +0200

    UPDATE

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "SaverioMazza",
+	url: "https://example.com",
+	description: "Nextjs 14 digital garden using velite, tailwind and shadcn",
+	author: "SaverioMazza",
+	links: {
+		twitter: "https://twitter.com/SaverioMazza5",
+		github: "https://github.com/mazzasaverio",
+
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
