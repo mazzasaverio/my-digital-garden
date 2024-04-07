f3e8a82787e7bf66423f97935d7c022e860bd17c
mazzasaverio <saverio3107@gmail.com>
2024-04-07 22:59:50 +0200

    UPDATE

---
+++ config/site-metadata.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+	name: "Saverio Mazza",
+	url: "https://example.com",
+	description: "Nextjs 14 digital garden using velite, tailwind and shadcn",
+	author: "Saverio Mazza",
+	links: {
+		twitter: "https://twitter.com/SaverioMazza5",
+		github: "https://github.com/mazzasaverio",
+
+	},
+	kbarConfig: {
+		searchDocumentsPath: "search.json",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
